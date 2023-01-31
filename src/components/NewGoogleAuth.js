8078509db60b6c9502d5e75c6f0f3a5f1e1788cc
Erika <erikaangela4@gmail.com>
2023-01-30 23:11:38 -0800

    Successful sign in and jwt token received

--- src/components/NewGoogleAuth.js
+++ src/components/NewGoogleAuth.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { useEffect } from "react";
 
+import jwt_decode from "jwt-decode";
+
 const NewGoogleAuth = () => {
+  /* global google */
+
   function handleCallbackResponse(response) {
     console.log("Encoded JWT ID token " + response.credential);
+    let userObject = jwt_decode(response.credential);
+    console.log(userObject);
   }
   useEffect(() => {
-    /* global google */
     google.accounts.id.initialize({
       client_id:
         "878755791416-577fukk9g6gm0ruspbhhi0crfoh3s8va.apps.googleusercontent.com",
