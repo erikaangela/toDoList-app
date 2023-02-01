41ced74e00724d34e2dc8e5cd27176b8f26ab951
Erika <erikaangela4@gmail.com>
2023-01-31 22:14:02 -0800

    Added window.google for global google

--- src/components/NewGoogleAuth.js
+++ src/components/NewGoogleAuth.js
@@ -12,16 +12,19 @@ const NewGoogleAuth = () => {
     console.log(userObject);
   }
   useEffect(() => {
-    google.accounts.id.initialize({
+    window.google.accounts.id.initialize({
       client_id:
         "878755791416-577fukk9g6gm0ruspbhhi0crfoh3s8va.apps.googleusercontent.com",
       callback: handleCallbackResponse,
     });
 
-    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
-      theme: "outline",
-      size: "large",
-    });
+    window.google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {
+        theme: "outline",
+        size: "large",
+      }
+    );
   }, []);
 
   return (
